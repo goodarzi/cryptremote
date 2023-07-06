004eb368d8ef9f7e51e7a78570f79e422b35893c
goodarzi <itsgoodarzi@gmail.com>
2023-07-07 00:42:38 +0330

    Add placeholders and configs.

--- cryptremote.sh
+++ cryptremote.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+set -e
+#set -x
+
+REMOTEFORWARDP=${REMOTEFORWARDP:-"2220"}
+REMOTEADDR=${REMOTEADDR:-"example.com"}
+REMOTEPORT=${REMOTEPORT:-"22"}
+REMOTEUSER=${REMOTEUSER:-"remoteuser"}
+LOCALPORT=${LOCALPORT:-"22"}
+OSIMAGE=${OSIMAGE:-"/usr/src/2023-05-03-raspios-bullseye-arm64-lite.img"}
+ROOTMOUNT=${ROOTMOUNT:-"/mnt/cryptroot"}
+ROOTIMAGE=${ROOTIMAGE:-"/dev/loop0p2"}
+BOOTIMAGE=${BOOTIMAGE:-"/dev/loop0p1"}
+CRYPTMAP=${CRYPTMAP:-"crypt"}
+SERVERPUBKEY=${SERVERPUBKEY:-"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI.... remote@example.com"}
+USERPUBKEY=${USERPUBKEY:-"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI.... user@example.local"}
 
 mountimage(){
     existing_loop=$(losetup -j ${OSIMAGE})
@@ -94,3 +109,183 @@ dbinitramfs(){
     echo 'command="/etc/unluks.sh; exit" '${USERPUBKEY} >> ${ROOTMOUNT}/etc/dropbear-initramfs/authorized_keys
     cat ${ROOTMOUNT}/etc/dropbear-initramfs/authorized_keys
 }
+
+
+
+initramfs(){
+cat << _EOF_ > ${ROOTMOUNT}/etc/initramfs-tools/hooks/zz-cryptsetup 
+#!/bin/sh
+set -e
+
+PREREQ=""
+prereqs()
+{
+	echo "\${PREREQ}"
+}
+
+case "\${1}" in
+	prereqs)
+		prereqs
+		exit 0
+		;;
+esac
+
+. /usr/share/initramfs-tools/hook-functions
+
+mkdir -p \${DESTDIR}/cryptroot || true
+cat /etc/crypttab >> \${DESTDIR}/cryptroot/crypttab
+cat /etc/fstab >> \${DESTDIR}/cryptroot/fstab
+cat /etc/crypttab >> \${DESTDIR}/etc/crypttab
+cat /etc/fstab >> \${DESTDIR}/etc/fstab
+
+_EOF_
+
+chmod +x ${ROOTMOUNT}/etc/initramfs-tools/hooks/zz-cryptsetup
+
+grep -q dm_crypt ${ROOTMOUNT}/etc/initramfs-tools/modules || echo dm_crypt >> ${ROOTMOUNT}/etc/initramfs-tools/modules
+
+echo CRYPTSETUP=y >> ${ROOTMOUNT}/etc/cryptsetup-initramfs/conf-hook
+
+cat << _EOF_ > ${ROOTMOUNT}/etc/initramfs-tools/unluks.sh
+#!/bin/sh
+
+export PATH='/sbin:/bin:/usr/sbin:/usr/bin'
+
+if [ \${1+1} ]; then
+    echo -e \$1'\n' | cryptsetup luksOpen /dev/disk/by-uuid/${CRYPTUUID} crypt
+else
+    while true; do
+        test -e /dev/mapper/${CRYPTMAP} && break
+        cryptsetup luksOpen /dev/disk/by-uuid/${CRYPTUUID} crypt
+        [ \$? -eq 0 ] && sleep 2 || exit 1
+    done
+fi
+
+if test -e /dev/mapper/${CRYPTMAP}; then
+    /scripts/local-top/cryptroot
+    for i in \$(ps aux | grep 'cryptroot' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    for i in \$(ps aux | grep 'askpass' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    for i in \$(ps aux | grep 'ask-for-password' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    for i in \$(ps aux | grep 'dbclient' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    for i in \$(ps aux | grep 'authback' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    for i in \$(ps aux | grep '\\-sh' | grep -v 'grep' | awk '{print \$1}'); do kill -9 \$i; done
+    exit 0
+fi
+exit 1
+
+_EOF_
+
+chmod +x ${ROOTMOUNT}/etc/initramfs-tools/unluks.sh
+
+cat << _EOF_ > ${ROOTMOUNT}/etc/initramfs-tools/scripts/init-premount/authback
+#!/bin/sh
+
+PREREQ="dropbear"
+prereqs()
+{
+	echo "\${PREREQ}"
+}
+ 
+case "\${1}" in
+	prereqs)
+		prereqs
+		exit 0
+		;;
+esac
+
+. /scripts/functions
+
+check_internet(){
+	if ping -c 1 google.com > /dev/null 2>&1; then
+	    echo "online"
+	else
+	    echo "offline"
+	fi
+}
+
+create_link(){
+	echo "LINK UP: Waiting for the network config"
+	while :; do
+		if [[ \$(check_internet) == "online" ]]; then 
+			break
+		fi
+		sleep 2 || exit
+	done
+	echo "Creating link with server..."
+	/sbin/ifconfig lo up
+	dbclient -R ${REMOTEFORWARDP}:127.0.0.1:${LOCALPORT} ${REMOTEUSER}@${REMOTEADDR} -p ${REMOTEPORT} -i /root/.ssh/dropbear_ed25519_host_key -y -T 
+}
+
+watchdog(){
+	echo "Watchdog started for network config"
+	sleep 60
+
+	if [[ \$(check_internet) == "online" ]]; then
+		echo "Internet connection OK: stopping the short watchdog,"
+		echo "...setting long watchdog (10 minutes)."
+		sleep 600
+	else
+		echo "No internet connection, rebooting..."
+		sleep 3
+	fi
+	test -e /dev/mapper/${CRYPTMAP} && exit 0 || /sbin/reboot -f || exit
+}
+
+create_link &
+watchdog &
+
+_EOF_
+
+chmod +x ${ROOTMOUNT}/etc/initramfs-tools/scripts/init-premount/authback
+
+
+cat << _EOF_ > ${ROOTMOUNT}/etc/initramfs-tools/hooks/zz-dbclient
+#!/bin/sh
+set -e
+
+PREREQ="dropbear"
+prereqs()
+{
+     echo "\$PREREQ"
+}
+ 
+case \$1 in
+prereqs)
+     prereqs
+     exit 0
+     ;;
+esac
+
+. /usr/share/initramfs-tools/hook-functions
+
+# Begin real processing below this line
+
+copy_exec /usr/bin/dbclient /bin
+SSH_DIR="\${DESTDIR}/root/.ssh/"
+mkdir -p \$SSH_DIR
+cp /etc/dropbear-initramfs/dropbear_ed25519_host_key \$SSH_DIR
+
+
+LIB=/lib/aarch64-linux-gnu
+mkdir -p "\$DESTDIR/\$LIB"
+cp \$LIB/libnss_dns.so.2 \\
+  \$LIB/libnss_files.so.2 \\
+  \$LIB/libresolv.so.2 \\
+  \$LIB/libc.so.6 \\
+  "\${DESTDIR}/\$LIB"
+echo nameserver 8.8.8.8 > "\${DESTDIR}/etc/resolv.conf"
+copy_file shell /etc/initramfs-tools/unluks.sh /etc/unluks.sh
+
+_EOF_
+
+chmod +x ${ROOTMOUNT}/etc/initramfs-tools/hooks/zz-dbclient
+
+cp ${ROOTMOUNT}/usr/share/initramfs-tools/scripts/init-premount/dropbear \
+${ROOTMOUNT}/etc/initramfs-tools/scripts/init-premount/dropbear
+
+sed -i '/^.*!= nfs/a sleep 5' ${ROOTMOUNT}/etc/initramfs-tools/scripts/init-premount/dropbear
+
+}
+
+
+${func} 
